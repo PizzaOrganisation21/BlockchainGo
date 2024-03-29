5a17e5f46033e5a5108d76dde6e1ce6a2fa08b1e
BusinessCheddar <164792020+BusinessCheddar@users.noreply.github.com>
2024-03-29 15:49:27 +0100

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/goTest
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	test "example.com/goTest/Test"
+)
+
+// Soll "Dinge" Rückmelden
+func main() {
+	fmt.Println(test.Test())
+}
